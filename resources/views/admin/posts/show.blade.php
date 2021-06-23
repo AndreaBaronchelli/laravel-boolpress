4d52a8f872a55214de5206805794192150c8a012
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-06-23 22:38:03 +0200

    add types to show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,16 +4,23 @@
 
     <div class="container text-center">
         <header class="mb-3">
-            <h1>{{$post->title}}</h1>
-            <small>{{$post->author}}</small>
             @if ($post->category)
-                <div class="badges">
-                    <span class="badge badge-info">{{$post->category->name}}</span>
+                <div class="badges d-flex justify-content-between">
+                    <h3><span class="badge badge-info">{{$post->category->name}}</span></h3>
+                    <div class="types">
+                        @foreach ($post->types as $type)
+                            <span class="badge badge-secondary px-2">{{$type->name}}</span>
+                        @endforeach
+                    </div>  
+                    
                 </div>
             @endif
+            <h1>{{$post->title}}</h1>
         </header>
         
         <p>{{$post->content}}</p>
+
+        <div class="author mb-3">{{$post->author}}</div>
         <a href="{{route('admin.posts.index')}}">Back to Archive</a>
     </div>
 
